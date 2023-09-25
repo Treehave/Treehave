12f8604a1045a64104f03f3e3712efcb22c3ad63
Andrew Everage-Scheible <aleveragesch@bsu.edu>
2023-09-25 16:50:56 -0400

    Create addon

---
+++ project/addons/treehave/treehave.gd
@@ -0,0 +1,24 @@
+@tool
+extends EditorPlugin
+
+
+# A class member to hold the dock during the plugin life cycle.
+var dock
+
+
+func _enter_tree():
+	# Initialization of the plugin goes here.
+	# Load the dock scene and instantiate it.
+	dock = preload("res://addons/treehave/editor_dock.tscn").instantiate()
+
+	# Add the loaded scene to the docks.
+	add_control_to_bottom_panel(dock, "Treehave")
+	# Note that LEFT_UL means the left of the editor, upper-left dock.
+
+
+func _exit_tree():
+	# Clean-up of the plugin goes here.
+	# Remove the dock.
+	remove_control_from_bottom_panel(dock)
+	# Erase the control from the memory.
+	dock.free()
