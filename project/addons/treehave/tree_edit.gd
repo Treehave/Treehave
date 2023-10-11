8db04677770a8911f42d8bcd484196a16513b089
Andrew Everage-Scheible <andreweveragescheible@gmail.com>
2023-10-11 04:26:20 -0400

    Fix erroneous comment and rename node variables

--- project/addons/treehave/tree_edit.gd
+++ project/addons/treehave/tree_edit.gd
@@ -7,20 +7,20 @@ func _process(_delta):
 
 
 func _draw():
-	var first_node: GraphNode
-	var second_node: GraphNode
+	var from_node: GraphNode
+	var to_node: GraphNode
 
 	for connection in get_connection_list():
-		# Search through children for nodes with matching names and assign indexes
+		# Search through children for nodes with matching names and assign nodes
 		for node in get_children():
 			if node.name == connection.get("from"):
-				first_node = node
+				from_node = node
 
 			elif node.name == connection.get("to"):
-				second_node = node
+				to_node = node
 		# Draw one line per connection
-		draw_line(_calculate_center_position(first_node), 
-					_calculate_center_position(second_node), Color.BLUE, 5, true)
+		draw_line(_calculate_center_position(from_node), 
+					_calculate_center_position(to_node), Color.BLUE, 5, true)
 
 
 func _calculate_center_position(control: Control) -> Vector2:
