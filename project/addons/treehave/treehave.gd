26668c2d1d03c92d5d1584598e567814a5def729
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-10-16 12:36:45 -0400

    Add centering to graph node spacing
    The nodes are now centered to their parent. They can still overlap with other nodes children

--- project/addons/treehave/treehave.gd
+++ project/addons/treehave/treehave.gd
@@ -1,50 +1,57 @@
 @tool
 extends Control
 
+
+const X_SPACING := 240.0
+const Y_SPACING := 160.0
+
 var _node_spawn_button_preload := preload(
 				"res://addons/treehave/graph_node_spawn_button.tscn")
 var _graph_node_preset := preload(
 				"res://addons/treehave/preset_nodes/graph_node_preset.tscn")
-var _current_behavior_tree : BeehaveTree
+var _current_behavior_tree: BeehaveTree
 
 @onready var _graph_edit: GraphEdit = %GraphEdit
 @onready var _file_dialog: FileDialog = %FileDialog
 @onready var _panel_vbox: VBoxContainer = %PanelVBox
 
 
-func set_tree(tree: BeehaveTree):
+func set_tree(tree: BeehaveTree) -> void:
 	_current_behavior_tree = tree
 	_clear_current_graph()
 	_build_current_tree_graph()
 
 
-func _clear_current_graph()->void:
+func _clear_current_graph() -> void:
+	_graph_edit.clear_connections()
 	# Delete all children of the GraphEdit.
 	for node in _graph_edit.get_children():
 		node.queue_free()
 
 
-func _build_current_tree_graph()->void:
+func _build_current_tree_graph() -> void:
 	# Translates the beehave tree represented by _current_behavior_tree into a graph.
 	var root_node := _create_graph_node(_current_behavior_tree)
-	_add_graph_for_children(_current_behavior_tree, root_node, 1)
+	_build_graph_node(_current_behavior_tree.get_child(0), root_node)
 
 
-func _add_graph_for_children(root: Node, parent_graph: GraphNode, tree_level: int)->void:
-	# For each child of root, create a graph node and connect it to parent_graph
+func _build_graph_node(node: Node, parent_graph_node: GraphNode) -> void:
+	if node == null:
+		return
 
-	for child in root.get_children():
-		var graph_node := _create_graph_node(child)
+	var graph_node := _create_graph_node(node)
 
-		_graph_edit.connect_node(parent_graph.name, 0, graph_node.name, 0)
+	_graph_edit.connect_node(parent_graph_node.name, 0, graph_node.name, 0)
 
-		var child_number = child.get_index()
-		_set_graph_node_position(graph_node, child_number, tree_level, parent_graph.get_position_offset())
+	var child_number := node.get_parent().get_children().find(node)
+	var child_count := node.get_parent().get_child_count()
+	_set_graph_node_position(graph_node, parent_graph_node, child_number, child_count)
 
-		_add_graph_for_children(child, graph_node, tree_level + 1)
+	for child in node.get_children():
+		_build_graph_node(child, graph_node)
 
 
-func _create_graph_node(from: Node)->GraphNode:
+func _create_graph_node(from: Node) -> GraphNode:
 	# Create a new graph node with the same name and title as "from" and return it
 	var graph_node := _graph_node_preset.instantiate()
 	graph_node.title = from.name
@@ -53,11 +60,11 @@ func _create_graph_node(from: Node)->GraphNode:
 	return graph_node
 
 
-func _set_graph_node_position(graph_node: GraphNode, child_number: int, tree_level: int, parent_position_offset: Vector2)-> void:
-	var x_offset = parent_position_offset.x + child_number * 200.0
-	var y_offset = tree_level * 150.0
-	graph_node.set_position_offset(Vector2(x_offset, y_offset))
+func _set_graph_node_position(graph_node: GraphNode, parent_graph_node: GraphNode, child_number: int, child_count: int)-> void:
+	var x_offset := parent_graph_node.position_offset.x + (child_number - (child_count - 1) / 2.0) * X_SPACING
+	var y_offset := parent_graph_node.position_offset.y + Y_SPACING
 
+	graph_node.set_position_offset(Vector2(x_offset, y_offset))
 
 func _on_graph_edit_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	_graph_edit.connect_node(from_node, from_port, to_node, to_port)
@@ -82,10 +89,10 @@ func _on_add_scene_button_pressed() -> void:
 
 
 func _on_file_dialog_file_selected(path: String) -> void:
-	var _path_array : Array = path.split("/")
-	var _node_name = _path_array[_path_array.size() - 1]
+	var _path_array: Array[String] = path.split("/")
+	var _node_name := _path_array[_path_array.size() - 1]
 
-	var _node_spawn_button = _node_spawn_button_preload.instantiate()
+	var _node_spawn_button := _node_spawn_button_preload.instantiate()
 	_node_spawn_button.set_text(_node_name)
 	_node_spawn_button.node_path = path
 	_node_spawn_button.graph_edit = _graph_edit
