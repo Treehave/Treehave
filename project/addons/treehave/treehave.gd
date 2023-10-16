ba65e2dbe505b7c7b6fca096f92021a1982bf159
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-10-16 14:00:50 -0400

    Implement properly spaced tree arrangment

--- project/addons/treehave/treehave.gd
+++ project/addons/treehave/treehave.gd
@@ -11,6 +11,8 @@ var _graph_node_preset := preload(
 				"res://addons/treehave/preset_nodes/graph_node_preset.tscn")
 var _current_behavior_tree: BeehaveTree
 
+var _node_to_graph_node_map: Dictionary = {}
+
 @onready var _graph_edit: GraphEdit = %GraphEdit
 @onready var _file_dialog: FileDialog = %FileDialog
 @onready var _panel_vbox: VBoxContainer = %PanelVBox
@@ -20,35 +22,38 @@ func set_tree(tree: BeehaveTree) -> void:
 	_current_behavior_tree = tree
 	_clear_current_graph()
 	_build_current_tree_graph()
+	_arrange_current_tree_graph()
 
 
 func _clear_current_graph() -> void:
+	_node_to_graph_node_map.clear()
 	_graph_edit.clear_connections()
 	# Delete all children of the GraphEdit.
 	for node in _graph_edit.get_children():
 		node.queue_free()
 
 
+func _get_graph_node(node: Node) -> GraphNode:
+	return _node_to_graph_node_map[node]
+
+
 func _build_current_tree_graph() -> void:
 	# Translates the beehave tree represented by _current_behavior_tree into a graph.
-	var root_node := _create_graph_node(_current_behavior_tree)
-	_build_graph_node(_current_behavior_tree.get_child(0), root_node)
+	_create_graph_node(_current_behavior_tree)
+	_build_graph_node(_current_behavior_tree.get_child(0))
 
 
-func _build_graph_node(node: Node, parent_graph_node: GraphNode) -> void:
+func _build_graph_node(node: Node) -> void:
 	if node == null:
 		return
 
+	var parent_graph_node := _get_graph_node(node.get_parent())
 	var graph_node := _create_graph_node(node)
 
 	_graph_edit.connect_node(parent_graph_node.name, 0, graph_node.name, 0)
 
-	var child_number := node.get_parent().get_children().find(node)
-	var child_count := node.get_parent().get_child_count()
-	_set_graph_node_position(graph_node, parent_graph_node, child_number, child_count)
-
 	for child in node.get_children():
-		_build_graph_node(child, graph_node)
+		_build_graph_node(child)
 
 
 func _create_graph_node(from: Node) -> GraphNode:
@@ -57,14 +62,75 @@ func _create_graph_node(from: Node) -> GraphNode:
 	graph_node.title = from.name
 	graph_node.set_name(from.name)
 	_graph_edit.add_child(graph_node)
+	_node_to_graph_node_map[from] = graph_node
+
 	return graph_node
 
 
-func _set_graph_node_position(graph_node: GraphNode, parent_graph_node: GraphNode, child_number: int, child_count: int)-> void:
-	var x_offset := parent_graph_node.position_offset.x + (child_number - (child_count - 1) / 2.0) * X_SPACING
-	var y_offset := parent_graph_node.position_offset.y + Y_SPACING
+func _arrange_current_tree_graph() -> void:
+	# Arranges the graph nodes in the GraphEdit.
+	var node := _current_behavior_tree.get_child(0)
+	_arrange_graph_node(node)
 
-	graph_node.set_position_offset(Vector2(x_offset, y_offset))
+
+func _arrange_graph_node(node: Node) -> void:
+	if node == null:
+		return
+
+	var queue := []
+	queue.append(node)
+
+	while queue.size() > 0:
+		var current_node := queue.pop_front()
+
+		_set_graph_node_position(current_node)
+
+		for child in current_node.get_children():
+			queue.append(child)
+
+
+func _set_graph_node_position(node: Node) -> void:
+	var parent_node := node.get_parent()
+	var graph_node := _get_graph_node(node)
+	var parent_graph_node := _get_graph_node(parent_node)
+	var sibling_index := parent_node.get_children().find(node)
+	var sibling_count := parent_node.get_child_count()
+	var width := _get_node_width(node)
+	var pre_width := 0
+	var post_width := 0
+	var parent_width := 0
+
+	for i in range(0, sibling_index):
+		pre_width += _get_node_width(parent_node.get_child(i))
+
+	for i in range(sibling_index + 1, sibling_count):
+		post_width += _get_node_width(parent_node.get_child(i))
+
+	parent_width = pre_width + width + post_width
+
+	var x_offset := 0.0
+	var y_offset := Y_SPACING
+
+	var left_most_x_offset := -parent_width * X_SPACING / 2.0
+
+	x_offset = left_most_x_offset + (width * X_SPACING) / 2.0 + (pre_width * X_SPACING)
+
+	var final_x_offset := parent_graph_node.get_position_offset().x + x_offset
+	var final_y_offset := parent_graph_node.get_position_offset().y + y_offset
+	graph_node.set_position_offset(Vector2(final_x_offset, final_y_offset))
+
+
+func _get_node_width(node: Node, depth := -1) -> int:
+	if node == null or node.get_child_count() == 0 or depth == 0:
+		return 1
+
+	var width := 0
+
+	for child in node.get_children():
+		width += _get_node_width(child, depth - 1)
+
+	return width
+
 
 func _on_graph_edit_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	_graph_edit.connect_node(from_node, from_port, to_node, to_port)
